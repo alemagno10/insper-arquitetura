0b377002f44858fbc95ff528f6c996eddf24eff0
alemagno10 <alemagno03@gmail.com>
2023-04-06 11:13:40 -0300

    13

--- aula-13/partida/src/test/java/com/insper/partida/aposta/BetControllerTests.java
+++ aula-13/partida/src/test/java/com/insper/partida/aposta/BetControllerTests.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.insper.partida.equipe.Team;
 import com.insper.partida.equipe.TeamController;
 import com.insper.partida.equipe.TeamService;
+import com.insper.partida.game.Game;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,21 +41,45 @@ public class BetControllerTests {
                 .build();
     }
 
+    private static Bet getBet(){
+        Bet bet = new Bet();
+        bet.setId(1);
+        bet.setGame(getGame());
+        return bet;
+    }
+
+    private static Game getGame(){
+        Game g = new Game();
+        return g;
+    }
+
     @Test
     void test_listBets() throws Exception {
+        Bet b = getBet();
 
+        List<Bet> bets = new ArrayList<>();
+        bets.add(b);
+
+        Mockito.when(betService.listBets()).thenReturn(bets);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.get("/bet"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        ObjectMapper om = new ObjectMapper();
+
+        String resp = result.getResponse().getContentAsString();
+        Assertions.assertEquals(om.writeValueAsString(bets), resp);
     }
 
     @Test
     void test_createBets() throws Exception {
-
+        
     }
 
     @Test
     void test_verifyBets() throws Exception {
 
     }
-
-
-
 }
